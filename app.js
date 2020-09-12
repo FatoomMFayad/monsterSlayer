826de48b6f0cbd77b79adaa4368abfe6266fc4c4
Fatoom Fayad <fatoomfayyad@gmail.com>
2020-09-12 14:33:39 +0300

    add styling and show the game log

--- app.js
+++ app.js
@@ -3,31 +3,40 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
-        attack: function(){
-            this.monsterHealth = this.calculateDamage(3, 10); 
-            if(this.checkWin()){
-               return; 
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
+            if (this.checkWin()) {
+                return;
             }
-            this.monsterAttacks();       
+
+            this.monsterAttacks();
         },
-        specialAttack: function(){
-            this.monsterHealth = this.calculateDamage(10, 20); 
-            if(this.checkWin()){
-               return; 
-            } 
-            this.monsterAttacks(); 
-        },
-        monsterAttacks: function(){
-            this.playerHealth = this.calculateDamage(5, 12);; 
-            this.checkWin();
+        specialAttack: function () {
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
         },
         heal: function(){
             if(this.playerHealth <= 90){
@@ -35,30 +44,44 @@ new Vue({
             }else{
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
             this.monsterAttacks();
         },
-        giveUp: function(){
-
+        giveUp: function () {
+            this.gameIsRunning = false;
         },
-        calculateDamage: function(min, max){
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
+        },
+        calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
-        checkWin: function(){
-            if(this.monsterHealth <= 0) {
-                if(confirm('You won! New Game?')){
-                 this.startGame();   
-                }else{
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
                     this.gameIsRunning = false;
                 }
                 return true;
-            }else if(this.playerHealth <=0) {
-                if(confirm('You lost! New Game?')){
-                    this.startGame();   
-                   }else{
-                       this.gameIsRunning = false;
-                   }
-                   return false;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
             }
+            return false;
         }
     }
-})
+});
